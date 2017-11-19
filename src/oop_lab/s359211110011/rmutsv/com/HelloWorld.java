dc674a2e02a60359c36b38fe84ab6f2e1cd9e455
natpatchamook20 <natpatcha2012@gmail.com>
2017-11-19 14:50:00 +0700

    Testing upload to github

---
+++ src/oop_lab/s359211110011/rmutsv/com/HelloWorld.java
@@ -0,0 +1,15 @@
+package oop_lab.s359211110011.rmutsv.com;
+
+public class HelloWorld {
+
+//    create main_method
+public static void main(String[] args) {
+
+    System.out.println("Hello World, I' am MOOK");
+
+
+
+} // main
+
+
+}// class
