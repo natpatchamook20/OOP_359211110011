36bb177896b5deee178b9f7e41dbc45a26af46ad
natpatchamook20 <natpatcha2012@gmail.com>
2017-11-29 11:42:52 +0700

    testing calgrade&condition

---
+++ src/ooplab3/TestLoopFor.java
@@ -0,0 +1,34 @@
+package ooplab3;
+
+public class TestLoopFor {
+
+    public static void main (String[] args) {
+        //Loop for
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.print("Enter your score: ");
+
+            if(i%3==0||i%5==0||i%7==0)
+                System.out.print(i+"  ");
+
+
+
+
+        }//for
+
+    }
+
+
+
+
+
+
+
+
+
+}//class
+
+
+
+
+
--- src/ooplab3/calgrade1.java
+++ src/ooplab3/calgrade1.java
@@ -16,16 +16,29 @@ public class calgrade1 {
 
 
     public static void main (String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner =new Scanner(System.in);
         int score;
-        System.out.print("Enter your score: ");
-        score = scanner.nextInt();
+        int count =0;
 
-        if (score < 49) System.out.print("Your grade is F");
-        else if (score < 59) System.out.print("Your grade is D");
-        else if (score < 69) System.out.print("Your grade is C");
-        else if (score < 79) System.out.print("Your grade is B");
-        else System.out.print("Your grade is A");
+      for (int i=1;i<=3;i++)
+        System.out.print("Enter your score (0-100): ");
+        score = scanner.nextInt();
+        if (score < 0 || score > 100) {
+            count++;
+            if (count == 3) {
+                System.out.print("System Error.");
+                //break;
+            }
+        }
+        else{
+            if (score < 50) System.out.print("Your grade is F");
+            else if (score <= 59) System.out.print("Your grade is D");
+            else if (score <= 69) System.out.print("Your grade is C");
+            else if (score <= 79) System.out.print("Your grade is B");
+            else System.out.print("Your grade is A");
+
+           // break;
+        }
     }
 
     }
