76971f457305c91351d4480a82d7fd69c92dec8d
natpatchamook20 <natpatcha2012@gmail.com>
2017-11-29 10:10:59 +0700

    testing Calgrabe1

---
+++ src/ooplab3/calgrade1.java
@@ -0,0 +1,31 @@
+//condition
+//   score 0-49 grade F
+// score 50-59 grade D
+//  score 60-69 grade C
+//  score 70-79 grade B
+//  score 80-100 grade A
+
+
+
+package ooplab3;
+
+import java.util.Scanner;
+
+public class calgrade1 {
+
+
+
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int score;
+        System.out.print("Enter your score: ");
+        score = scanner.nextInt();
+
+        if (score < 49) System.out.print("Your grade is F");
+        else if (score < 59) System.out.print("Your grade is D");
+        else if (score < 69) System.out.print("Your grade is C");
+        else if (score < 79) System.out.print("Your grade is B");
+        else System.out.print("Your grade is A");
+    }
+
+    }
