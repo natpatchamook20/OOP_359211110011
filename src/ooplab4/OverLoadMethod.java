7faacec4690c6ef2ad487f0616c72446dd7ed675
natpatchamook20 <natpatcha2012@gmail.com>
2017-12-06 11:44:46 +0700

    Testing OverLoadMethod

--- src/ooplab4/OverLoadMethod.java
+++ src/ooplab4/OverLoadMethod.java
@@ -2,27 +2,24 @@ package ooplab4;
 //Overlosding method
 
 public class OverLoadMethod {
-    private static int number =100;
-    public static void Summation(int x,int y){
-
-        System.out.println(x+y);
-        int t = number+x+y;
-
+    public static int number = 100;
+    public static void Summation(int x,int y) {
+               System.out.println(x+y);
+        int t = number + x + y;
     }
-    public static void  Summation(){
-        System.out.println("Hello"+number);
+    public static void Summation (){
+                System.out.println("Hello"+number);
+            }
+     public static void Summation(int x,int y,int z){
+                System.out.println(x+y+z+number);
+            }
+     public static void main(String[] args) {
+                Summation();
+                Summation(5,5);
+                Summation(3,5,7);
 
-    }
-    public static void Summation(int x,int y,int z){
-        System.out.println(x+y+z+number);
-
-    }
-    public static void main(String[] args){
-        Summation();
-        Summation(x:10,y:10);
-        Summation(x:5,y:5,z:5);
-    }
-
+                    }//main
 
 
-}
+
+         }//class
